218177af39fd960c065ca2295244948aca2784b0
Francis Patron <francis.patron1@upr.edu>
2021-10-03 20:14:03 -0300

    added auth stuff

--- global_pet_sitter/global_pet_sitter/settings.py
+++ global_pet_sitter/global_pet_sitter/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
